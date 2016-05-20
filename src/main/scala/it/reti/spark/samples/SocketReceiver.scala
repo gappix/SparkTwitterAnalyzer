7ce55c70123e0566d0b331dc8678dcbf29d03058
Andrea Biancini <andrea.biancini@gmail.com>
2016-05-20 12:27:01 +0200

    Modified receiver for socket error

--- src/main/scala/it/reti/spark/samples/SocketReceiver.scala
+++ src/main/scala/it/reti/spark/samples/SocketReceiver.scala
@@ -11,45 +11,50 @@ import java.net.Socket
 class SocketReceiver(port: Int)
   extends Receiver[String](StorageLevel.MEMORY_AND_DISK_2) with Logging {
 
-  var serverSocket : ServerSocket = null;
+  var serverSocket : ServerSocket = null
   
   def onStart() {
-    serverSocket = new ServerSocket(port);
+    serverSocket = new ServerSocket(port)
+    
     // Start the thread that receives data over a connection
     new Thread("Socket Receiver") {
-      override def run() { receive() }
+      override def run() {
+        while (!isStopped) {
+          receive()
+        }
+      }
     }.start()
   }
 
   def onStop() {
     // There is nothing much to do as the thread calling receive()
     // is designed to stop by itself if isStopped() returns false
-    serverSocket.close();
-    serverSocket = null;
+    serverSocket.close()
+    serverSocket = null
   }
 
   /** Create a socket connection and receive data until receiver is stopped */
   private def receive() {
-    var socket : Socket = serverSocket.accept();
-    var userInput: String = null
-    
     try {
-     // Until stopped or connection broken continue reading
-     val reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"))
-     userInput = reader.readLine()
-     while(!isStopped && userInput != null) {
-       store(userInput)
-       userInput = reader.readLine()
-     }
-     reader.close()
-
-    } catch {
-     case e: java.net.ConnectException =>
-       // restart if could not connect to server
-       restart("Error connecting to port " + port, e)
-     case t: Throwable =>
-       // restart if there is any other error
-       restart("Error receiving data", t)
+      // Until stopped or connection broken continue reading
+      val socket : Socket = serverSocket.accept()
+      val reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"))
+      
+      var userInput: String = null
+      do { 
+        userInput = reader.readLine()
+        if (userInput != null) store(userInput)
+      }
+      while(!isStopped && userInput != null)
+      
+      socket.close()
+     } catch {
+       case e: java.net.ConnectException =>
+         // restart if could not connect to server
+         restart("Error connecting to port " + port, e)
+       case t: Throwable =>
+         // restart if there is any other error
+         restart("Error receiving data", t)
     }
   }
 }
